267ea9983f7766faaf95cfdfc75f150bf8163e32
Samadhan <katkarsamadhan5@gmail.com>
2022-11-03 16:08:58 +0530

    new code added

--- Test.java
+++ Test.java
@@ -1,5 +1,14 @@
 Public class Test{
+<<<<<<< Updated upstream
 piblic void Add(){
 System.out.println("Add Method")
 
+=======
+public void main (String [] args){
+int a=1;
+int b=2;
+int c=a+b;
+System.out.printl(c);
+}
+>>>>>>> Stashed changes
 }
